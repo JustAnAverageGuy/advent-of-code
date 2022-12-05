54c4dc06aad284429939dd0947d8a7b00c585a44
JustAnAverageGuy <oceanic.arnav@gmail.com>
2022-12-05 16:12:45 +0530

    Add a crates parser taken from reddit u\wzkx

--- AoC2022/day_05.py
+++ AoC2022/day_05.py
@@ -1,6 +1,3 @@
-import aoc_helper
-import re
-raw = aoc_helper.fetch(5, 2022)
 from aoc_helper import (
     Grid,
     PrioQueue,
@@ -14,18 +11,39 @@ from aoc_helper import (
     range,
     tail_call,
 )
+import aoc_helper
+import re
+raw = aoc_helper.fetch(5, 2022)
 
+"""
+# this function could have been used to avoid manual parsing of input
+
+def parse_crates(f):
+    lines = []
+    while '1' not in (line := f.readline()):
+        lines.append(line)
+    n = int(line.strip().split()[-1])
+    crates = []
+    for i in range(n):
+        crates.append([])
+    while lines:
+        line = lines.pop()
+        for i in range(n):
+            p = 1 + 4*i
+            if p < len(line) and line[p] != ' ':
+                crates[i].append(line[p])
+    return crates
+"""
 
 def parse_raw():
-    crates,steps = raw.split("\n\n")
-    steps = list(map(extract_ints,steps.split('\n')))
-    crates = [[],['P', 'F', 'M', 'Q', 'W', 'G', 'R', 'T'], ['H', 'F', 'R'], ['P', 'Z', 'R', 'V', 'G', 'H', 'S', 'D'], ['Q', 'H', 'P', 'B', 'F', 'W', 'G'], ['P', 'S', 'M', 'J', 'H'], ['M', 'Z', 'T', 'H', 'S', 'R', 'P', 'L'], ['P', 'T', 'H', 'N', 'M', 'L'], ['F', 'D', 'Q', 'R'], ['D', 'S', 'C', 'N', 'L', 'P', 'H']]# manually quicker
-    return crates,steps
-    
-        
+    crates, steps = raw.split("\n\n")
+    steps = list(map(extract_ints, steps.split('\n')))
+    crates = [[], ['P', 'F', 'M', 'Q', 'W', 'G', 'R', 'T'], ['H', 'F', 'R'], ['P', 'Z', 'R', 'V', 'G', 'H', 'S', 'D'], ['Q', 'H', 'P', 'B', 'F', 'W', 'G'], [
+        'P', 'S', 'M', 'J', 'H'], ['M', 'Z', 'T', 'H', 'S', 'R', 'P', 'L'], ['P', 'T', 'H', 'N', 'M', 'L'], ['F', 'D', 'Q', 'R'], ['D', 'S', 'C', 'N', 'L', 'P', 'H']]  # manually quicker
+    return crates, steps
 
 
-crates,steps = parse_raw() # steps: move %d from %d to %d
+crates, steps = parse_raw()  # steps: move %d from %d to %d
 
 
 def part_one():
@@ -33,8 +51,7 @@ def part_one():
         t = crates[i[1]][-i[0]:]
         crates[i[1]] = crates[i[1]][:-i[0]]
         crates[i[2]].extend(t[::-1])
-    return ''.join(map(lambda x:x[-1] if len(x) else '' ,crates))
-        
+    return ''.join(map(lambda x: x[-1] if len(x) else '', crates))
 
 
 def part_two():
@@ -42,7 +59,7 @@ def part_two():
         t = crates[i[1]][-i[0]:]
         crates[i[1]] = crates[i[1]][:-i[0]]
         crates[i[2]].extend(t)
-    return ''.join(map(lambda x:x[-1] if len(x) else '' ,crates))
+    return ''.join(map(lambda x: x[-1] if len(x) else '', crates))
 
 
 aoc_helper.lazy_submit(day=5, year=2022, solution=part_one)
