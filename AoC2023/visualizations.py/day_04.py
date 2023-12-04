a697e6732b418d349bb4f1a63eccffde9a48df03
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-04 18:06:55 +0530

    Refactor \(smol\)

--- AoC2023/visualizations.py/day_04.py
+++ AoC2023/visualizations.py/day_04.py
@@ -3,7 +3,9 @@ from colorist import *
 import time
 import sys
 from math import log2
+
 DELAY = "DELAY" in sys.argv
+EXAMPLE = "EXAMPLE" in sys.argv
 
 try:
 
@@ -18,9 +20,9 @@ try:
         if delay: time.sleep(delay_time)
         
 
-    data = aoc_helper.fetch(4,2023)
+    data = aoc_helper.fetch(4,2023) if not EXAMPLE else aoc_helper.get_sample_input(day=4,part=1,year=2023)[0] 
 
-    N = len(data)
+    N = len(data.splitlines())
     tot_counts = {i: 1 for i in range(N)}
     tot_score = 0
     for idx,i in enumerate(data.splitlines()):
@@ -30,35 +32,33 @@ try:
         mine = aoc_helper.extract_ints(smine)
         intersect = set(win) & set(mine)
         
-        for hmm in range(2):
-            if not hmm:
-                pprint(sid,end=': ')
-                for j in win: pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ')
-                pprint('|',end=' ')
-                for j in mine:
-                    pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ')
-                pprint('|',end='\r')
-            else:
-                pprint(sid,end=': ', delay_time=0.01)
-                for j in win:
-                    if j in intersect: pprint(f'{BrightColor.YELLOW}{j:2}{BrightColor.OFF}',end=' ', delay_time=0.01)
-                    else: pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ', delay_time=0.01)
-                pprint('|',end=' ', delay_time=0.01)
-                for j in mine:
-                    if j in intersect: pprint(f'{BrightColor.YELLOW}{j:2}{BrightColor.OFF}',end=' ', delay_time=0.01)
-                    else: pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ', delay_time=0.01)
-                
-                common = len(intersect)
-                score = (1 << (common - 1)) if common else 0
-                if common:
-                    for k in range(idx+1,idx+common+1):
-                        if k >= N: break
-                        tot_counts[k] += tot_counts[idx]
-                tot_score += score
-                if common: 
-                    pprint(f'| {Color.GREEN}{common:2}{Color.OFF} -> {Effect.DIM}{score:3}{Effect.DIM_OFF} [{Effect.DIM}{tot_counts[idx]:^8}{Effect.DIM_OFF}] {BrightColor.MAGENTA}{("#"*int(log2(tot_counts[idx]))):<30}{BrightColor.OFF}', delay=False)
-                else:
-                    pprint(f'|           [{Effect.DIM}{tot_counts[idx]:^8}{Effect.DIM_OFF}] {Effect.DIM}{Color.MAGENTA}{("#"*int(log2(tot_counts[idx]))):<30}{Color.OFF}{Effect.DIM_OFF}', delay=False)
+        pprint(sid,end=': ')
+        for j in win: pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ')
+        pprint('|',end=' ')
+        for j in mine:
+            pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ')
+        pprint('|',end='\r')
+        
+        pprint(sid,end=': ', delay_time=0.01)
+        for j in win:
+            if j in intersect: pprint(f'{BrightColor.YELLOW}{j:2}{BrightColor.OFF}',end=' ', delay_time=0.01)
+            else: pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ', delay_time=0.01)
+        pprint('|',end=' ', delay_time=0.01)
+        for j in mine:
+            if j in intersect: pprint(f'{BrightColor.YELLOW}{j:2}{BrightColor.OFF}',end=' ', delay_time=0.01)
+            else: pprint(f'{Effect.DIM}{j:2}{Effect.DIM_OFF}',end=' ', delay_time=0.01)
+        
+        common = len(intersect)
+        score = (1 << (common - 1)) if common else 0
+        if common:
+            for k in range(idx+1,idx+common+1):
+                if k >= N: break
+                tot_counts[k] += tot_counts[idx]
+        tot_score += score
+        if common: 
+            pprint(f'| {Color.GREEN}{common:2}{Color.OFF} -> {Effect.DIM}{score:3}{Effect.DIM_OFF} [{Effect.DIM}{tot_counts[idx]:^8}{Effect.DIM_OFF}] {BrightColor.MAGENTA}{("#"*int(log2(tot_counts[idx]))):<30}{BrightColor.OFF}', delay=False)
+        else:
+            pprint(f'|           [{Effect.DIM}{tot_counts[idx]:^8}{Effect.DIM_OFF}] {Effect.DIM}{Color.MAGENTA}{("#"*int(log2(tot_counts[idx]))):<30}{Color.OFF}{Effect.DIM_OFF}', delay=False)
 
     pprint('_'*140)
     pprint(
